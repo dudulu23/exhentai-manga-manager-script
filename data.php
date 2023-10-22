0000d4ee8a5140a131dc5eb3b25f94c3cb09fefc
dudulu23 <wang949885554@qq.com>
2023-10-22 15:38:08 +0800

    1.4
    1.4

--- data.php
+++ data.php
@@ -5,6 +5,7 @@ header('Access-Control-Allow-Origin:*');
 
 $fun = $_POST['fun'];
 $appAppDatapath=$_POST['appAppDatapath'];
+$metadatapath=$_POST['metadatapath'];
 if($fun=='showDownload'){
     $ehid = $_POST['ehid'];
     showDownload($ehid,$appAppDatapath);
@@ -12,82 +13,104 @@ if($fun=='showDownload'){
 if($fun=='updateDatabase'){
     $gid= $_POST['gid'];
     $metaobj = $_POST['metaobj'];
-    updateDatabase($metaobj,$gid,$appAppDatapath);
+    updateDatabase($metaobj,$gid,$appAppDatapath,$metadatapath);
 }
    
 
 function showDownload($jsonString,$appAppDatapath){
-
         // 将 JSON 字符串转换为 PHP 数组
         $data = json_decode($jsonString, true);
         $back = [];
         // 检查是否成功解析 JSON
         if (json_last_error() === JSON_ERROR_NONE) {
-    try {
-        // 创建 PDO 实例，连接到 SQLite 数据库文件
-        $pdo = new PDO('sqlite:'.$appAppDatapath);
+            try {
+                // 创建 PDO 实例，连接到 SQLite 数据库文件
+                          
+                $appAppDatapath='sqlite:'.$appAppDatapath;
+ 
 
-        // 设置 PDO 错误模式为异常
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $pdo = new PDO($appAppDatapath);
 
-        // 执行查询语句
-        foreach($data as $i){
-            $stmt = $pdo->query("SELECT * FROM `Mangas` WHERE url LIKE '%".$i."%'");
-            // 获取结果集中的数据
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                array_push($back,$i);
+                // $pdo = new PDO($appAppDatapath);
+
+                // 设置 PDO 错误模式为异常
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                // 执行查询语句
+                foreach($data as $i){
+                    $stmt = $pdo->query("SELECT * FROM `Mangas` WHERE url LIKE '%".$i."%'");
+                    // 获取结果集中的数据
+                    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                        array_push($back,$i);
+                    }
+                }
+                $back_str=json_encode($back);
+                echo $back_str;
+                
+            } catch (PDOException $e) {
+                echo "数据库连接失败：" . $e->getMessage();
             }
-        }
-        $back_str=json_encode($back);
-        echo $back_str;
-        
-    } catch (PDOException $e) {
-        echo "数据库连接失败：" . $e->getMessage();
-    }
         } else {
             // JSON 解析错误
             echo "Failed to decode JSON.";
         }
 }
 //更新meta
-function updateDatabase($metaobj,$gid,$appAppDatapath){
-    
+function updateDatabase($metaobj,$gid,$appAppDatapath,$metadatapath){
+    echo $metadatapath;
     try {
         $metaobj = json_decode($metaobj, true);
         $metaobj['tags']=json_encode($metaobj['tags']);
         // 创建 PDO 实例，连接到 SQLite 数据库文件
         $pdo = new PDO('sqlite:'.$appAppDatapath);
+
+        $pdo_meta = new PDO('sqlite:'.$metadatapath);
         // 设置 PDO 错误模式为异常
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql_update="UPDATE Mangas SET title = '".$metaobj['title']."', rating = '".$metaobj['rating']."', url = '".$metaobj['url']."', title_jpn = '".$metaobj['title_jpn']."', posted = '".$metaobj['posted']."', filecount = '".$metaobj['filecount']."', category = '".$metaobj['category']."', filesize = '".$metaobj['filesize']."', status = '".$metaobj['status']."', tags = '".$metaobj['tags']."' WHERE id =";
+        $pdo_meta->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        $sql_update="UPDATE Mangas SET title = '".$metaobj['title']."', rating = '".$metaobj['rating']."', url = '".$metaobj['url']."', title_jpn = '".$metaobj['title_jpn']."', posted = '".$metaobj['posted']."', filecount = '".$metaobj['filecount']."', category = '".$metaobj['category']."', filesize = '".$metaobj['filesize']."', status = '".$metaobj['status']."', tags = '".$metaobj['tags']."' WHERE hash =";
+        $sql_update_meta="UPDATE Metadata SET title = '".$metaobj['title']."', rating = '".$metaobj['rating']."', url = '".$metaobj['url']."', title_jpn = '".$metaobj['title_jpn']."', posted = '".$metaobj['posted']."', filecount = '".$metaobj['filecount']."', category = '".$metaobj['category']."', filesize = '".$metaobj['filesize']."', status = '".$metaobj['status']."', tags = '".$metaobj['tags']."' WHERE hash =";
+
         // 搜索gid
         //三次对比，成功就就跳出判断保留row
         $stmt = $pdo->query("SELECT * FROM `Mangas` WHERE filepath LIKE '%\\".$gid."-%'");
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        if($row['id']==null){
+        if($row['hash']==null){
+
             $stmt = $pdo->query("SELECT * FROM `Mangas` WHERE filepath LIKE '%".$metaobj['title']."%'");
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
-            if($row['id']==null){
-                $stmt = $pdo->query("SELECT * FROM `Mangas` WHERE filepath LIKE '%".$metaobj['title_jpn']."%'");
-                $row= $stmt->fetch(PDO::FETCH_ASSOC);       
+            if($row['hash']==null){
+                //title_jpn不能为空
+                if($metaobj['title_jpn']!=""){
+                    $stmt = $pdo->query("SELECT * FROM `Mangas` WHERE filepath LIKE '%".$metaobj['title_jpn']."%'");
+                    $row= $stmt->fetch(PDO::FETCH_ASSOC); 
+                    
+                }
+                echo $row['hash'];      
             }
         }
-        if($row['id']==null){
+        if($row['hash']==null){
+            
             echo "notfind";
         }else{
             try {
                 //寻找之前结果的id
-                $affectedRows = $pdo->exec($sql_update."'".$row['id']."'");
-                echo "succsee";
+             
+                $pdo->exec($sql_update."'".$row['hash']."'");
+                $pdo_meta->exec($sql_update_meta."'".$row['hash']."'");
+
+                echo "success";
             } catch (PDOException $e) {
-                echo "error";
+                echo "error 1";
+
             }
             
         }        
            
       
         
-    } catch (PDOException $e) {echo "error";}
+    } catch (PDOException $e) {echo "error 2"; echo $e;}
             
 }
 
--- ehentai显示已下载 提取页面元数据.user.js
+++ ehentai显示已下载 提取页面元数据.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ehentai显示已下载 提取页面元数据
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @license      MIT
 // @description  exhentai-manga-manager的附加浏览器脚本
 // @author       You
@@ -56,15 +56,62 @@ text-align: center;
     color:#fff;
 
 }
+.showDialog{
+      white-space: nowrap;
+      position:fixed;
+      background-color: #444;
+      border-radius: 10px;
+      padding:18.7px;
+      z-index:99;
+      color:#fff;
+      left: 50%;
+      transform: translate(-50%,0);
+      vertical-align: middle;
+      display:none;
+  }
+  .showDialog div
+  .right{
+
+  }
+  .inputtext{
+    width: 400px;
+  }
+  .pointer{
+cursor: pointer;
+  }
     `);
 
     'use strict';
+    $("body").prepend("<div class='showDialog'><div><label>端口号<input type='text' class='right inputtext port'></label></div> <br> <div> <label> <span title='点击选择文件路径' class='pointer'>database路径</span> <input type='file' style='display: none;' id='database' name='file'> <input type='text' class='right inputtext databasepath' name=''> </label> </div> <br> <div> <label> <span title='点击选择文件路径' class='pointer'>metabase路径</span> <input type='file' style='display: none;' id='metabase' name='file'> <input type='text' class='right inputtext metadatapath' > </label> </div> <br> <div class='right'> <button class='confim'>保存</button> <button class='close'>关闭</button> </div> </div>");
+
     $("body").prepend("<a class='setting'></a>");
     $("body").prepend("<div class='copyed'></div>");
+    $(".showDialog .close").click(function(){
+        $(".showDialog").hide();
+    });
+    $(".showDialog .confim").click(function(){
+        if( ($(".showDialog .databasepath").val()!='')&&($(".showDialog .port").val()!='')&&($(".showDialog .metadatapath").val()!='')){
+            if(confirm("要保存当前配置吗？")){
+                   setting("del");
+                   setting("add");
+            }
+        }else{
+            alert("配置不完整");
+        }
+
+    });
+    $('#database').change(function() {
+        $(".showDialog .databasepath").val(this.value);
+    });
+    $('#metabase').change(function() {
+        $(".showDialog .metadatapath").val(this.value);
+    });
     var ehid;
     var url=window.location.href;
     var result_port;
     var result_appAppDatapath;
+    var result_metadatapath;
+    var metadatapath;
     const request = indexedDB.open('ex1', 1);
     request.onupgradeneeded = function(event) {
         const db = event.target.result;
@@ -91,27 +138,36 @@ text-align: center;
 
         store.get("port").onsuccess = function(event) {
             result_port = event.target.result;
-            console.log(result_port);
+        
+
+        };
+        store.get("metadatapath").onsuccess = function(event) {
+            result_metadatapath = event.target.result;
 
         };
 
-
         transaction.oncomplete = function() {
-            //数据库为空时
-            if((result_appAppDatapath==undefined)||(result_port==undefined)){
+            //端口路径为空时
+            if((result_appAppDatapath==undefined)||(result_port==undefined)||(result_metadatapath==undefined)){
                 $(".setting").html("没有设置端口或数据库文件路径，点击设置");
                 $(".setting").click(function(){
-                    setting('add');
+                    $(".showDialog").show(50);
 
                 });
             }else{
+               
                 $(".setting").html("已设置端口和数据库路径");
+
                 //删除indexdb
                 $(".setting").click(function(){
-                    if(confirm("是否要清空端口和路径")){
-                        setting('del');
+                    $(".showDialog .databasepath").val(result_appAppDatapath['val']);
+                    $(".showDialog .port").val(result_port['val']);
+                    $(".showDialog .metadatapath").val(result_metadatapath['val']);
+                    $(".showDialog").show();
+//                     if(confirm("是否要清空端口和路径")){
+//                         setting('del');
 
-                    }
+//                     }
 
                 });
                 //主功能开始
@@ -125,9 +181,9 @@ text-align: center;
                     $.ajax({
                         url: "http://localhost:"+result_port['val']+"/data.php",  // 请求的URL
                         method: "POST",  // 请求方法，可选项包括 "GET", "POST", "PUT", "DELETE" 等.
-                        dataType:'json',
                         data:{
                             appAppDatapath:result_appAppDatapath['val'],
+                            metadatapath:result_metadatapath['val'],
                             ehid:ehid,
                             fun:'showDownload'
                         },  // 请求参数，如果不需要传递参数可以省略
@@ -146,11 +202,12 @@ text-align: center;
                     });
                     //点击后添加到数据库
                     $(".mark").click(function(){
-                        updateSqlite(url,result_appAppDatapath['val'],result_port['val'])
+                        updateSqlite(url,result_appAppDatapath['val'],result_port['val'],result_metadatapath['val'])
                     });
                 }
                 //e站搜索页面
-                if(url.indexOf("hentai.org/?f_search")!=-1){
+                if(url.indexOf("hentai.org")!=-1&&$("#toppane").length==1){
+
                     ehid=[];
                     $("#f_search").contextmenu(function(){
                         $("#f_search").val("");
@@ -169,7 +226,7 @@ text-align: center;
                     });
                     $(".gl1t").append("<div class='mark'><div style='color:#999;text-align: center;'>未连接</div></div>");
                     $(".mark").click(function(){
-                        updateSqlite($(this).parent().find("a").attr("href"),result_appAppDatapath['val'],result_port['val'])
+                        updateSqlite($(this).parent().find("a").attr("href"),result_appAppDatapath['val'],result_port['val'],result_metadatapath['val'])
                     });
                     $(".gl1t").each(function(){
                         var ehurl=$(this).find("a").attr("href");
@@ -181,6 +238,7 @@ text-align: center;
                         method: "POST",  // 请求方法，可选项包括 "GET", "POST", "PUT", "DELETE" 等.
                         data: {
                             appAppDatapath:result_appAppDatapath['val'],
+                            result_metadatapath:result_metadatapath['val'],
                             ehid:JSON.stringify(ehid),
                             fun:'showDownload'
                         },  // 请求参数，如果不需要传递参数可以省略
@@ -188,14 +246,20 @@ text-align: center;
                             //转为对象
                             var back_obj = eval(response);
                             $(".mark").html("<div style='color:#999;text-align: center;'>未下载</div>");
+
                             //遍历网址
                             $(".gl1t").each(function(){
                                 //遍历返回的对象
                                 for(let i=0;i<back_obj.length;i++){
                                     if($(this).find("a").attr("href").indexOf(back_obj[i])!=-1){
-                                        $(this).find(".mark").html("<div style='color:#1c1;text-align: center;'>已下载</div>")
+                                        $(this).find(".mark").html("<div style='color:#1c1;text-align: center;'>已下载</div>");
                                         break;
                                     }
+                                }
+                                //当前页面只有一个搜素结果时自动提取meta
+                                if($(".gl1t").length==1&& $(".mark").text()=="未下载"){
+                                    $(".mark").focus();
+                                    $(".mark").trigger("click");
                                 }
                             });
                         },
@@ -204,11 +268,7 @@ text-align: center;
                             console.log("请求错误:", error);
                         }
                     });
-                    //当前页面只有一个搜素结果时自动提取meta
-                    if($(".gl1t").length==1&&($(".mark").html()=="未下载")){
-                        $(".mark").focus();
-                        $(".mark").trigger("click");
-                    }
+                    
                 }
             }
 
@@ -221,7 +281,7 @@ text-align: center;
     };
 
     //更新数据库
-    function updateSqlite(url,appAppDatapath,port){
+    function updateSqlite(url,appAppDatapath,port,metadatapath){
         //获取元数据
         let match = /(\d+)\/([a-z0-9]+)/.exec(url)
         fetch('https://api.e-hentai.org/api.php', {
@@ -265,6 +325,7 @@ text-align: center;
                 method: "POST",
                 data:{
                     appAppDatapath:appAppDatapath,
+                    metadatapath:metadatapath,
                     gid:gid,
                     metaobj:JSON.stringify(metaobj),
                     fun:'updateDatabase'
@@ -276,7 +337,7 @@ text-align: center;
                         navigator.clipboard.writeText(JSON.stringify(metaobj));
                         $(".copyed").text("录入失败，请手动录入元数据，元数据已复制到剪切版");
                     }
-                    if(response.indexOf("succsee")!=-1){
+                    if(response.indexOf("success")!=-1){
                         $(".copyed").text("录入成功");
                     }
                     $('.copyed').fadeIn(); // 元素开始时显示
@@ -314,15 +375,19 @@ text-align: center;
             // 获取对象存储空间
             const store = transaction.objectStore('setting');
             if(type=='add'){
-                var port = prompt('请输入php服务端口号');
-                var appAppDatapath= prompt('请输入database.sqlite的路径         安装版exhentai-manga-manager的数据库文件一般在     C:\\Users\\用户名\\AppData\\Roaming\\exhentai-manga-manager下');
+                var port = $(".showDialog .port").val();
+                var appAppDatapath= $(".showDialog .databasepath").val();
+                var metadatapath=$(".showDialog .metadatapath").val();
                 const data_port = { name:'port', val: port };
                 store.add(data_port);
                 const data_appAppDatapath = { name:'appAppDatapath', val: appAppDatapath };
                 store.add(data_appAppDatapath);
+                const data_metadatapath = { name:'metadatapath', val: metadatapath };
+                store.add(data_metadatapath);
             }else if(type=='del'){
                 store.delete('port');
                 store.delete('appAppDatapath');
+                store.delete('metadatapath');
             }
             transaction.oncomplete = function() {
                 // 关闭数据库连接
